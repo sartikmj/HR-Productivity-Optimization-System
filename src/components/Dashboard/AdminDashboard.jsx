95a57f0137cf3e65d6aefc52a94280581c04ff63
sartikmj <sartikmj@gmail.com>
2025-03-24 23:41:27 +0530

    Admin Dashboard Form Made

--- src/components/Dashboard/AdminDashboard.jsx
+++ src/components/Dashboard/AdminDashboard.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Header from '../../other/Header'
 
 const AdminDashboard = () => {
   return (
-    <div></div>
+    <div className='h-screen w-full p-10'>
+        <Header />
+        <div>
+            <form>
+                <div>
+                <h3>Task Title</h3>
+                <input type="text" placeholder='Make a UI Design' />
+                </div>
+                <div>
+                <h3>Description</h3>
+                <textarea name="" placeholder='Write your description' cols="30" rows="10"></textarea>
+                </div>
+                <div>
+                <h3>Date</h3>
+                <input type="date" />
+                </div>
+                <div>
+                <h3>Assign To</h3>
+                <input type="text"  placeholder='Employee Name'/>
+                </div>
+                <div>
+                <h3>Category</h3>
+                <input type="text" placeholder='design, dev, etc' />
+                </div>
+                <div>
+                <button>Create Task</button>
+                </div>
+            </form>
+        </div>
+    </div>
   )
 }
 
