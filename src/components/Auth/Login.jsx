fb1ed32dc12ec1501baf0dc66b9727b35e7702c2
sartikmj <sartikmj@gmail.com>
2025-03-24 23:05:08 +0530

    Employee Dashboard Created

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -4,13 +4,13 @@ const Login = () => {
     //we need the form not to be submited when the user clicks the button , we will use Two Way Binding
     //We need to change the default behavior of the form , i.e. to reload the page after submitting the form
     //Two Way Binding
-    const [email,setemail] = useState('')
-    const [password,setPassword] = useState('')
+    const [email, setemail] = useState('')
+    const [password, setPassword] = useState('')
 
-    const submitHandler = (e)=>{
+    const submitHandler = (e) => {
         e.preventDefault() //this prevent its default behaior . that reloads the page after submiting
-        console.log("email is",email)
-        console.log("password is",password)
+        console.log("email is", email)
+        console.log("password is", password)
 
         setemail('')
         setPassword('') //this will clear the input fields after submitting
@@ -19,25 +19,25 @@ const Login = () => {
     return (
         <div className='flex h-screen w-screen items-center justify-center' >
             <div className='border-2 border-emerald-600 p-20 rounded-xl'>
-                <form 
-                onSubmit={(e)=>{
-                    submitHandler(e)
-                }}
-                className='flex flex-col items-center justify-center'>
+                <form
+                    onSubmit={(e) => {
+                        submitHandler(e)
+                    }}
+                    className='flex flex-col items-center justify-center'>
 
                     <input
-                    value={email}
-                    onChange={(e)=>{
-                        setemail(e.target.value)
-                    }}
-                    required type="email" placeholder='Enter your email' className='text-white outline-none bg-transparent border-2 border-emerald-600 py-4 px-5 text-xl rounded-full placeholder:text-gray-400' />
-                    
-                    <input 
-                    value={password}
-                    onChange={(e)=>{
-                        setPassword(e.target.value)
-                    }}
-                    required type="password" placeholder='Enter password' className='text-white outline-none bg-transparent border-2 border-emerald-600 py-4 px-5 mt-3 text-xl rounded-full placeholder:text-gray-400' />
+                        value={email}
+                        onChange={(e) => {
+                            setemail(e.target.value)
+                        }}
+                        required type="email" placeholder='Enter your email' className='text-white outline-none bg-transparent border-2 border-emerald-600 py-4 px-5 text-xl rounded-full placeholder:text-gray-400' />
+
+                    <input
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value)
+                        }}
+                        required type="password" placeholder='Enter password' className='text-white outline-none bg-transparent border-2 border-emerald-600 py-4 px-5 mt-3 text-xl rounded-full placeholder:text-gray-400' />
                     <button className='text-white bg-emerald-600 mt-5 py-3 px-5 rounded-full'>Log in</button>
                 </form>
             </div>
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10'>
+        <div className='flex-shrink-0 h-full w-[350px] bg-red-400 p-5 rounded-xl '> 
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-small px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>26 Feb 2025</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Complete React Project</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quasi cum dolore recusandae sapiente nihil?
+            </p>
+        </div>
+        
+        <div className='flex-shrink-0 h-full w-[350px] bg-blue-400 p-5 rounded-xl '> 
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-small px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>26 Feb 2025</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Complete React Project</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quasi cum dolore recusandae sapiente nihil?
+            </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[350px] bg-green-400 p-5 rounded-xl '> 
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-small px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>26 Feb 2025</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Complete React Project</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quasi cum dolore recusandae sapiente nihil?
+            </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[350px] bg-yellow-400 p-5 rounded-xl '> 
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-small px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>26 Feb 2025</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Complete React Project</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quasi cum dolore recusandae sapiente nihil?
+            </p>
+        </div>
+        <div className='flex-shrink-0 h-full w-[350px] bg-pink-400 p-5 rounded-xl '> 
+            <div className='flex justify-between items-center'>
+                <h3 className='bg-red-600 text-small px-3 py-1 rounded'>High</h3>
+                <h4 className='text-sm'>26 Feb 2025</h4>
+            </div>
+            <h2 className='mt-5 text-2xl font-semibold'>Complete React Project</h2>
+            <p className='text-sm mt-2'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quasi cum dolore recusandae sapiente nihil?
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default TaskList
