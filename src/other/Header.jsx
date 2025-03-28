575f3be4c04b48bbd73b02cd39326b772e92a8db
sartikmj <sartikmj@gmail.com>
2025-03-28 12:06:47 +0530

    created logout feature

--- src/other/Header.jsx
+++ src/other/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { setLocalStorage } from '../utils/LocalStorage'
 
 const Header = () => {
 
@@ -11,10 +12,15 @@ const Header = () => {
   //   setUsername(data.firstName)
   // }
 
+  const logOutUser = (()=>{
+    localStorage.setItem('loggedInUser','')
+    window.location.reload(); //function to reload the page
+  })
+
   return (
     <div className='text-white flex items-end justify-between'>
         <h1 className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>username👋🏻</span></h1>
-        <button className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-small'>Log Out</button>
+        <button onClick={logOutUser} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-small'>Log Out</button>
     </div>
   )
 }
