82faf41edf8f94f4c313549bbe728774190e8015
sartikmj <sartikmj@gmail.com>
2025-03-29 11:33:53 +0530

    creating tasks

--- src/other/AllTask.jsx
+++ src/other/AllTask.jsx
@@ -16,8 +16,8 @@ const AllTask = () => {
                 <h5 className='text-lg font-medium w-1/5 bg-purple-600 text-center'>Failed</h5>
             </div>
             <div className='h-[80%] overflow-auto'>
-            {authData.employees.map(function (elem) {
-                return  <div className='bg-black py-2 px-4 mb-2 flex justify-between rounded'>
+            {authData.employees.map(function (elem,idx) {
+                return  <div key={idx} className='bg-black py-2 px-4 mb-2 flex justify-between rounded'>
                 <h2 className='text-lg font-medium w-1/5 text-white text-center'>{elem.firstName}</h2>
                 <h3 className='text-lg font-medium w-1/5 text-blue-400 text-center'>{elem.taskNumbers.newTask}</h3>
                 <h5 className='text-lg font-medium w-1/5 text-yellow-400 text-center'>{elem.taskNumbers.active}</h5>
--- src/other/CreateTask.jsx
+++ src/other/CreateTask.jsx
@@ -1,34 +1,78 @@
 import React from 'react'
-
+import { useState } from 'react'
 const CreateTask = () => {
+
+    const [taskTitle, setTaskTitle] = useState('')
+    const [taskDescription, setTaskDescription] = useState('')
+    const [taskDate, setTaskDate] = useState('')
+    const [assignTo, setAssignTo] = useState('')
+    const [category, setCategory] = useState('')
+
+    const [task, setTask] = useState({})
+
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        
+        setTask({taskTitle,taskDescription,taskDate,category,active:false,newTask:true,failed:true,completed:false})
+
+        console.log(task)
+    }
+
   return (
     <div className='p-5 bg-[#1c1c1c] mt-7 rounded'>
-                <form className='flex flex-wrap w-full items-start justify-between' >
+                <form onSubmit={(e)=>{
+                    submitHandler(e);
+                }} className='flex flex-wrap w-full items-start justify-between' >
                     <div className='w-1/2'>
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5' >Task Title</h3>
-                            <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border[1px]  border-gray-400 mb-4 ' type="text" placeholder='Make a UI Design' />
+                            <input 
+                                value={taskTitle}
+                                onChange={(e)=>{
+                                    setTaskTitle(e.target.value); //e.target refers to the DOM element that triggered the event (in this case, the <input> element).
+                                }}
+                                className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border[1px]  border-gray-400 mb-4 ' type="text" placeholder='Make a UI Design' />
                         </div>
 
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5' >Date</h3>
-                            <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border[1px]  border-gray-400 mb-4 ' type="date" />
+                            <input 
+                                value={taskDate}
+                                onChange={(e)=>{
+                                    setTaskDate(e.target.value)
+                                }}
+                                className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border[1px]  border-gray-400 mb-4 ' type="date" />
                         </div>
 
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5' >Assign To</h3>
-                            <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border[1px]  border-gray-400 mb-4 ' type="text" placeholder='Employee Name' />
+                            <input 
+                                value={assignTo}
+                                onChange={(e)=>{
+                                    setAssignTo(e.target.value)
+                                }}
+                                className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border[1px]  border-gray-400 mb-4 ' type="text" placeholder='Employee Name' />
                         </div>
 
                         <div>
                             <h3 className='text-sm text-gray-300 mb-0.5' >Category</h3>
-                            <input className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border[1px] border-gray-400 mb-4' type="text" placeholder='design, dev, etc' />
+                            <input 
+                                value={category}
+                                onChange={(e)=>{
+                                    setCategory(e.target.value);
+                                }}
+                                className='text-sm py-1 px-2 w-4/5 rounded outline-none bg-transparent border[1px] border-gray-400 mb-4' type="text" placeholder='design, dev, etc' />
                         </div>
                     </div>
 
                     <div className='w-2/5 flex flex-col items-start'>
                         <h3 className='text-sm text-gray-300 mb-0.5' >Description</h3>
-                        <textarea className='w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400' name="" placeholder='Write your description' cols="30" rows="10"></textarea>
+                        <textarea 
+                            value={taskDescription} 
+                            onChange={(e)=>{
+                                setTaskDescription(e.target.value)
+                            }}
+                            className='w-full h-44 text-sm py-2 px-4 rounded outline-none bg-transparent border-[1px] border-gray-400' name="" placeholder='Write your description' cols="30" rows="10"></textarea>
                         <button className='bg-emerald-500 py-3 hover:bg-emerald-600 px-5 rounded text-sm mt-4 w-full' >Create Task</button>
                     </div>
 
